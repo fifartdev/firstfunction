a0d3f523840395e19585049b90874b3662c29bfa
fifartdev <fifartapp@gmail.com>
2024-02-19 17:27:39 +0200

    check active sessions await

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Client, Users } from "node-appwrite";
+import { Client, Account } from "node-appwrite";
 
 const client = new Client();
 
@@ -6,15 +6,11 @@ client
   .setEndpoint("https://fifartapp.eu/v1")
   .setProject("65cb29d9bd45ff3d39c2");
 
-const users = new Users(client);
-
-let activeUser = users.list();
+const account = new Account(client);
 
 export default async function ({ req, res, log, error }) {
   if (req.method === "GET") {
+    let activeUser = await account.get();
     return res.send(activeUser);
-  }
-  if (req.method === "GET") {
-    return log;
   }
 }
