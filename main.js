41864427af1df32da744d0717db6c81761f66c12
fifartdev <fifartapp@gmail.com>
2024-02-19 21:25:09 +0200

    POST updated request

--- main.js
+++ main.js
@@ -8,12 +8,16 @@ export default async function (context) {
     .setProject("65cb29d9bd45ff3d39c2");
 
   const databases = new Databases(client);
-  console.log(context);
+
   if (context.req.method === "POST") {
     const data = await databases.listDocuments(
       "65ccca601fd0e39ad991",
       "65ccca69938fb6387ea1"
     );
     return context.res.json(data.documents);
+  }
+
+  if (context.req.method === "GET") {
+    return context.res.json(context);
   }
 }
