63b668b28c7d8ea075ff3bd64e4766b66457eae4
fifartdev <fifartapp@gmail.com>
2024-02-19 17:09:40 +0200

    appwrite extra

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Client, Users } from "node-appwrite";
+import { Client } from "node-appwrite";
 
 const client = new Client();
 
@@ -7,8 +7,6 @@ client
   .setProject("65cb29d9bd45ff3d39c2");
 
 export default async function ({ req, res, log, error }) {
-  const userList = await users.list();
-
   if (req.method === "GET") {
     return res.send(client);
   }
