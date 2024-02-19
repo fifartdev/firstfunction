a8b3beb5cfcc9573a1e669c60e81710cbd589a39
fifartdev <fifartapp@gmail.com>
2024-02-19 21:23:52 +0200

    POST updated request

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { Client, Account, Databases } from "node-appwrite";
 
-export default async function ({ req, res, log, error }) {
+export default async function (context) {
   const client = new Client();
 
   client
@@ -8,12 +8,12 @@ export default async function ({ req, res, log, error }) {
     .setProject("65cb29d9bd45ff3d39c2");
 
   const databases = new Databases(client);
-
-  if (req.method === "POST") {
+  console.log(context);
+  if (context.req.method === "POST") {
     const data = await databases.listDocuments(
       "65ccca601fd0e39ad991",
       "65ccca69938fb6387ea1"
     );
-    return res.json(data.documents);
+    return context.res.json(data.documents);
   }
 }
