95973ad31742b83eb6d02759f2f0ab839e48940b
fifartdev <fifartapp@gmail.com>
2024-02-19 16:50:53 +0200

    corrected

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ export default async function ({ req, res, log, error }) {
   if (req.method === "GET") {
     return res.send("Hello Soulio");
   }
+  if (req.method === "GET") {
+    console.log("The", { req, res, log, error });
+  }
 }
