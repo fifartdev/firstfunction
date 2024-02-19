9bd169acbe9271abb6eabda1c8bb36ee1304b2dc
fifartdev <fifartapp@gmail.com>
2024-02-19 17:08:48 +0200

    appwrite added

--- main.js
+++ main.js
@@ -6,15 +6,11 @@ client
   .setEndpoint("https://fifartapp.eu/v1")
   .setProject("65cb29d9bd45ff3d39c2");
 
-const users = new Users(client);
-
 export default async function ({ req, res, log, error }) {
   const userList = await users.list();
 
   if (req.method === "GET") {
-    userList.map((u) => {
-      res.send("Hello ", u.email);
-    });
+    return res.send(client);
   }
   if (req.method === "GET") {
     return log;
