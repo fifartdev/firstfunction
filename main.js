82c1f5dd4db85c1dcf7b371f9a72aa60b1141304
fifartdev <fifartapp@gmail.com>
2024-02-19 17:00:13 +0200

    appwrite added

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+import { Client, Account } from "node-appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint("https://fifartapp.eu/v1")
+  .setProject("65cb29d9bd45ff3d39c2");
+
+const accounts = new Account(client);
+
 export default async function ({ req, res, log, error }) {
   if (req.method === "GET") {
-    return res.send("Hello Soulio");
+    const listSessions = accounts.listSessions();
+
+    return res.send("Hello Soulio", listSessions);
   }
   if (req.method === "GET") {
     return log;
